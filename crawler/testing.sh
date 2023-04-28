1bfc88075bc198841ea2b1e02e0ad6e208a962c1
Daisy Li <daisy.li.26@dartmouth.edu>
2023-04-28 18:21:52 -0400

    added crawler

---
+++ crawler/testing.sh
@@ -0,0 +1,27 @@
+# test command-line argument parsing
+
+# invalid number of arguments
+# ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ pgdir 1 2
+# ./crawler
+# ./crawler 1
+
+# maxDepth out of range
+# ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ pgdir 20
+
+# pageDirectory does not exist 
+# ./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ hello 2
+
+# seedURL is invalid 
+# ./crawler https://www.amazon.com/ pgdir 2
+
+# valid arguments; letters
+./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/index.html pgdir 3
+
+# valid arguments; letters
+#./crawler http://cs50tse.cs.dartmouth.edu/tse/letters/ pgdir 10
+
+# valid arguments; letters
+# ./crawler http://cs50tse.cs.dartmouth.edu/tse/toscrape/ 1
+
+# valid arguments; toScrape 
+# ./crawler http://cs50tse.cs.dartmouth.edu/tse/wikipedia/ pgdir 2
